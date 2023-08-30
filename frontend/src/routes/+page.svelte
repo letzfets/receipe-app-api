dd7eeb9497fed3d27ea4c7873b86f47605f78442
letzfets <arnold.knott@gmail.com>
2023-08-30 19:56:25 +0200

    ci: testing formating

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	const name = 'world';
+
+	console.log(`Hello ${name}!`     );
+</script>
+
 <h1>Welcome to SvelteKit - prepared setup for frontend for receipe app</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
