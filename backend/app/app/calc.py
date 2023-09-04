24b2933f4285eb4cbd044701fa9bc314a6d231be
letzfets <arnold.knott@gmail.com>
2023-09-04 19:15:19 +0200

    test: adds first test to backend

---
+++ backend/app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator Functions
+"""
+
+def add(x,y):
+    """Add two numbers together"""
+    return x + y
---
+++ backend/app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """Test adding two numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
