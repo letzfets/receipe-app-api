b7628971e9693a49325ca38ecfe5419c4f74ccd2
letzfets <arnold.knott@gmail.com>
2023-09-05 14:09:33 +0200

    test: adds testing-library to frontend and demos its usage

--- frontend/src/components/VerticalTabs.spec.ts
+++ frontend/src/components/VerticalTabs.spec.ts
@@ -1,21 +1,32 @@
-import { describe, it, expect } from 'vitest';
-import VerticalTabs from './VerticalTabs.svelte';
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/svelte'
+import VerticalTabs from './VerticalTabs.svelte'
 
 describe('VerticalTabs Component', () => {
-	it('should render the component', () => {
-		// Create a new container for the test
-		const host = document.createElement('div');
+    it('should render the component', () => {
+        render(VerticalTabs)
 
-		// Append the new container in the HTML body
-		document.body.appendChild(host);
+        const firstTabHeading = screen.getByText(/First Tab Heading/i)
 
-		// create an instance of the vertical tab
-		const instance = new VerticalTabs({ target: host });
+        expect(firstTabHeading).toBeTruthy()
+    })
+})
 
-		// Check if the instance has a value
-		expect(instance).toBeTruthy();
+// describe('VerticalTabs Component', () => {
+// 	it('should render the component', () => {
+// 		// Create a new container for the test
+// 		const host = document.createElement('div');
 
-		// Test if we can find the "First Tab Heading"
-		expect(host.innerHTML).toContain('First Tab Heading');
-	});
-});
+// 		// Append the new container in the HTML body
+// 		document.body.appendChild(host);
+
+// 		// create an instance of the vertical tab
+// 		const instance = new VerticalTabs({ target: host });
+
+// 		// Check if the instance has a value
+// 		expect(instance).toBeTruthy();
+
+// 		// Test if we can find the "First Tab Heading"
+// 		expect(host.innerHTML).toContain('First Tab Heading');
+// 	});
+// });
