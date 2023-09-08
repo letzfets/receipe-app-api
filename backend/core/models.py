7b985458eb77b01d51517781152bd461e75fd99e
letzfets <arnold.knott@gmail.com>
2023-09-08 22:39:14 +0200

    feat: adds normalization of email and test for it

--- backend/core/models.py
+++ backend/core/models.py
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save, and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         # responsible for the hashing of the password:
         user.set_password(password)
         # in case multiple databases are used:
