06e47a99576f7ede941e1190927c545290d6d7cd
letzfets <arnold.knott@gmail.com>
2023-09-09 10:00:17 +0200

    test: adds tests for listing users

---
+++ backend/core/tests/test_admin.py
@@ -0,0 +1,40 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test for Django admin"""
+
+    # unit-test module for Python uses camelCase
+    # whereas normal convention is snake_case
+    def setUp(self):
+        """Create User and client."""
+        self.client = Client()
+        # create a superuser
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="test123"
+        )
+        # login with the superuser
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="test123",
+            name="Test user",
+        )
+    
+    def test_users_list(self):
+        """Test that users are listed on user page."""
+        # generate url for list user page
+        url = reverse("admin:core_user_changelist")
+        # perform http get request
+        res = self.client.get(url)
+
+        # assertContains checks that the response contains a certain item
+        # and also checks that the http response is 200
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
