3e11ab628769d8b9990633a4988f7fad544ce568
letzfets <arnold.knott@gmail.com>
2023-09-09 11:33:16 +0200

    feat: customizes the fieldsets for the django admin user change page

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -4,6 +4,11 @@ Django admin customization
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# allows for translation of text:
+# https://docs.djangoproject.com/en/3.2/topics/i18n/translation/#module-django.utils.translation
+# (underscore is used as shortcut for gettext_lazy)
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -12,6 +17,29 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
+    # only access the fields, that are present in customized user model:
+    # overrides the default fields, which include:
+    # date_joined, username, first_name, last_name
+    # but those are not used in the customized user model.
+    fieldsets = (
+        # "None" stands for title
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (
+            _("Important dates"),
+            {"fields": ("last_login",)},
+        ),
+    )
+    readonly_fields = ["last_login"]
 
 
 admin.site.register(models.User, UserAdmin)
