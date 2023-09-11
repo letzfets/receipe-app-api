6b40e2ea1b61d150690cb682a3909beb9e257fcc
letzfets <arnold.knott@gmail.com>
2023-09-11 22:05:16 +0200

    test: resolves merge conflicts from code formating in cicd

--- frontend/src/lib/backend/generic.test.ts
+++ frontend/src/lib/backend/generic.test.ts
@@ -1,10 +1,25 @@
 // Tests for generic backend functions
-import { describe, it, expect } from 'vitest';
-import { waitForBackend } from './generic';
+import { describe, it, expect, beforeAll, afterAll, afterEach } from "vitest";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import { waitForBackend } from "./generic";
 
-describe('Tests for generic backend functions', () => {
-	it.todo('should wait for backend', async () => {
-		const result = await waitForBackend();
-		expect(result).toBe(true);
-	});
-});
+const server = setupServer();
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
+
+describe("Tests for generic backend functions", () => {
+    it.todo("should wait for backend", async () => {
+        server.use(
+            rest.get("https://backend/health", (req, res, ctx) => {
+                return res(
+                    ctx.status(200),
+                    ctx.delay(2000)
+                    );
+            })
+        );
+        const result = await waitForBackend();
+        expect(result).toBe(true);
+    })
+})
