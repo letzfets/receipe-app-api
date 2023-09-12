4b7c91f0949bea25abdeb9bd14dd8a92a5065bfe
letzfets <arnold.knott@gmail.com>
2023-09-12 23:14:22 +0200

    test: provides start of a test document to avoid failing test

--- frontend/src/lib/backend.test.ts
+++ frontend/src/lib/backend.test.ts
@@ -1,1 +1,8 @@
 // tests for backend functions
+import { describe, it, expect } from "vitest";
+
+describe("backend calls", () => {
+    it.todo("should be tested", () => {
+        expect(true).toBe(true);
+    });
+})
