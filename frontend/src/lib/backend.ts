011a0bdddaaa528daccdfff284ef4ea826e5c60d
letzfets <arnold.knott@gmail.com>
2023-09-12 22:58:07 +0200

    ci: updates frontend pacakages and resolves linting errors

--- frontend/src/lib/backend.ts
+++ frontend/src/lib/backend.ts
@@ -1,12 +1,14 @@
 // handle the backend API calls
 
 // TBD: GET from backend
-const getBackend = async (url: string) => {};
+// const getBackend = async (url: string) => {
+// }
+
 // TBD: POST to backend
 // TBD: PUT to backend
 // TBD: DELETE from backend
 
 // TBD: write test for getUser
-export const getUser = async () => {
-	await getBackend('https://backend/api/user');
-};
+// export const getUser = async () => {
+//     await getBackend('https://backend/api/user');
+// };
