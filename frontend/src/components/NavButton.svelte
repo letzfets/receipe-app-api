030af60c12abed42d922c8e7cd3ce63585e25e19
letzfets <arnold.knott@gmail.com>
2023-09-18 20:17:29 +0200

    fix: adds missing file

---
+++ frontend/src/components/NavButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let url: string;
+	export let link: string;
+</script>
+
+<a href={url}>
+	<button
+		type="button"
+		class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	>
+		{link}
+	</button>
+</a>
