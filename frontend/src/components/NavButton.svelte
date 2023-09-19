53127720348958d3268d71dfac84fb80fac62a14
letzfets <arnold.knott@gmail.com>
2023-09-19 21:20:48 +0200

    feat: adds user handling elements in upper right corner

--- frontend/src/components/NavButton.svelte
+++ frontend/src/components/NavButton.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	export let url: string;
 	export let link: string;
+	export let invert: boolean = false;
+
+	const formatButton = () => {
+		if (invert) {
+			return 'bg-white hover:bg-blue-700 text-blue-400 font-bold py-2 px-4 rounded border border-blue-400';
+		} else {
+			return 'bg-blue-400 hover:bg-blue-700 hover:text-white text-white font-bold py-2 px-4 rounded';
+		}
+	};
 </script>
 
 <a href={url}>
 	<button
 		type="button"
-		class="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		class={formatButton()}
 	>
 		{link}
 	</button>
