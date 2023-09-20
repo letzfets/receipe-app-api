2ec3f774a008cd0bec271e1bb3ef28b6c0f876ac
letzfets <arnold.knott@gmail.com>
2023-09-20 10:53:20 +0200

    feat: adds user form and mounts it on register route

---
+++ frontend/src/components/UserForm.spec.ts
@@ -0,0 +1,19 @@
+import { describe, test, expect } from 'vitest';
+
+describe('Login form', () => {
+	test.todo('should have a email and password fields', () => {
+		expect(1).toBe(1);
+	});
+});
+
+describe('Register form', () => {
+	test.todo('should have a email, password  and name fields', () => {
+		expect(1).toBe(1);
+	});
+});
+
+describe('User form', () => {
+	test.todo('should have prefilled email, password  and name fields', () => {
+		expect(1).toBe(1);
+	});
+});
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import UserForm from '$components/UserForm.svelte';
+</script>
+
+<UserForm />
