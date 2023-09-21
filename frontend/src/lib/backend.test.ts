a17d8a3b3abd2fc7dafc9f8de5ce21d28a340082
letzfets <arnold.knott@gmail.com>
2023-09-21 15:20:39 +0200

    test: adds test for post backend call

--- frontend/src/lib/backend.test.ts
+++ frontend/src/lib/backend.test.ts
@@ -2,7 +2,7 @@
 import { describe, test, expect, beforeAll, afterAll } from 'vitest';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
-import { getBackend } from './backend';
+import { getBackend, postBackend } from './backend';
 
 const dummyResponse = { data: 'test' };
 
@@ -21,4 +21,39 @@ describe('GET data from backend', () => {
 		const data = await getBackend('/api/schema?format=json');
 		expect(data).toEqual(dummyResponse);
 	});
+});
+
+describe('POST data to backend', () => {
+	test('should post data to backend', async () => {
+		const payload = {
+			name: 'User One',
+			email: 'user@example.com',
+			password: 'secretPassword'
+		};
+		server.use(
+			rest.post('http://host.docker.internal:8000/api/user/create', (req, res, ctx) => {
+				// return res((res) => {
+				// 	res.status = 200
+				// 	res.body = JSON.stringify({
+				// 		name: payload.name,
+				// 		email: payload.email
+				// 	})
+				// 	return res
+				// }
+				return res(
+					ctx.status(200),
+					ctx.json({
+						name: payload.name,
+						email: payload.email
+					})
+				);
+			})
+		);
+
+		const response = await postBackend('/api/user/create', payload);
+		// console.log(response);
+		// expect(response.status).toBe(200);
+		expect(response).toContain({ name: 'User One', email: 'user@example.com' });
+	});
+	test.todo('should read the posted data back from the backend');
 });
